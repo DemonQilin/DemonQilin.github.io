ecaffe265dc0190f346892847cadd7009db11a92
DemonQilin <velezloaizaesteban9012015@gmail.com>
2022-05-15 12:42:16 -0500

    Se Modifico la función observer del menú para que se ejecute solo en dispositivos con minimo 1024px

--- scripts/modules/observer-menu.js
+++ scripts/modules/observer-menu.js
@@ -1,27 +1,28 @@
 const d = document;
 
+let scrollSpy = new IntersectionObserver(markSection, {
+    threshold: 0.5
+});
+
+function markSection(entries) {
+    entries.forEach(entrie => {
+        if (entrie.isIntersecting) {
+            d.querySelector(`a.menu-link[href="#${entrie.target.id}"]`).classList.add('spy');
+        } else {
+            d.querySelector(`a.menu-link[href="#${entrie.target.id}"]`).classList.remove('spy');
+        }
+    })
+}
+
 export function observerMenu() {
     const $sections = d.querySelectorAll('[data-menu]');
     
-    let options = {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.5
-    }
-
-    let scrollSpy = new IntersectionObserver(markSection, options);
-
     $sections.forEach(section => {
         scrollSpy.observe(section);
     })
+}
 
-    function markSection(entries) {
-        entries.forEach(entrie => {
-            if (entrie.isIntersecting) {
-                d.querySelector(`a.menu-link[href="#${entrie.target.id}"]`).classList.add('spy');
-            } else {
-                d.querySelector(`a.menu-link[href="#${entrie.target.id}"]`).classList.remove('spy');
-            }
-        })
-    }
+export function deleteObserver() {
+    scrollSpy.disconnect();
+    d.querySelectorAll('a.menu-link[href]').forEach(el => el.classList.remove('spy'));
 }
